73b3bfe8e7b0568bce5265a62e120ed65e3629e8
harisdeathfire <haris.malik.7509@gmail.com>
2025-06-21 13:26:17 +0500

    Updated Testimonial Page and Styled it

--- Bayaan Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Bayaan Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a41887caa0c1e03bb5266363e2d828d219b834a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0698523123411d0391a131c0a28339734078be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
